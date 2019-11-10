9a6cffe2b75d3e48dc7746f79059cbbd897a8ac3
Hakeem Almidan <hakeemmidan@gmail.com>
2019-11-10 15:31:42 -0800

    Create store and root reducer

--- app/javascript/components/Root.jsx
+++ app/javascript/components/Root.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
 import { App } from './App';
+import { configureStore } from '../redux/store/store';
 
 
 const Root = () => (
-    // <Provider>
+    <Provider store={configureStore()}>
         <HashRouter>
             <App />
         </HashRouter>
-    // </Provider>
+    </Provider>
 )
 
 export default Root
