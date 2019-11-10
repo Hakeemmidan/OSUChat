bb9cdb625e6aae143f00e2806031743f292ad328
Hakeem Almidan <hakeemmidan@gmail.com>
2019-11-10 15:34:23 -0800

    Create users controller and jbuilder views

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,30 @@
+class Api::UsersController < ApplicationController
+    def index
+        @users = User.all
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            sign_in(@user)
+            render :index
+        else
+            render json: @user.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy
+        @user = User.find(params[:id])
+
+        if @user.destroy
+            render :index
+        else
+            render json: @user.errors.full_messages, status: 422
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:password, :username)
+    end
+end
