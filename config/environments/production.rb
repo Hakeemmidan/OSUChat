bb0757fa4734228b01681eb42b8d2f23270b8ee9
Hakeem Almidan <hakeemmidan@gmail.com>
2019-11-18 20:02:19 -0800

    Revert back to before job addition to avoid complications

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,12 +94,13 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for mailgun
-  config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :user_name            => ENV['gmail_username'],
-    :password             => ENV['gmail_password'],
-    :authentication       => "plain",
-    :enable_starttls_auto => true
-  }
+  debugger
+  ActionMailer::Base.smtp_settings = {
+    :port           => 587,
+    :address        => "smtp.mailgun.org",
+    :domain         => ENV['domain'],
+    :user_name      => ENV['username'],
+    :password       => ENV['password'],
+    :authentication => :plain,
+  }}
 end
