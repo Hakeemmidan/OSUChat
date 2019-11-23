06f5ee272659789c0c67eac0bdc222f800162183
Hakeem Almidan <hakeemmidan@gmail.com>
2019-11-22 16:36:11 -0800

    Add confirm_token route to app and link to sample_email view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, default: { format: :json } do
-    resources :users, only: [:create, :show]
+    resources :users, only: [:create, :show] do
+      member do
+        get :confirm_email
+      end
+    end
     resource :session, only: [:create, :destroy]
   end
   
