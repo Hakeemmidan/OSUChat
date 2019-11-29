a322dffd8d3aa393f0daf33f0cc14037d36fa614
Hakeem Almidan <hakeemmidan@gmail.com>
2019-11-29 15:49:26 -0800

    Add model methods

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,14 +1,14 @@
 class PasswordsController < ApplicationController
-    def forgot
-    if params[:email].blank? # check if email is present
+  def forgot
+    if params[:email].blank?
       return render json: {error: 'Email not present'}
     end
 
-    user = User.find_by(email: params[:email]) # if present find user by email
+    user = User.find_by(email: params[:email])
 
     if user.present?
-      user.generate_password_token! #generate pass token
-      # SEND EMAIL HERE
+      user.generate_password_token!
+      ExampleMailer.forgot_password_email(user.id).deliver_now
       render json: {status: 'ok'}, status: :ok
     else
       render json: {error: ['Email address not found. Please check and try again.']}, status: :not_found
--- test/mailers/previews/example_mailer_preview.rb
+++ test/mailers/previews/example_mailer_preview.rb
@@ -1,6 +1,10 @@
 # Preview all emails at http://localhost:3000/rails/mailers/example_mailer
 class ExampleMailerPreview < ActionMailer::Preview
-    def sample_mail_preview
-        ExampleMailer.sample_email(User.first)
+    def sample_email_preview
+        ExampleMailer.sample_email(User.first.id)
+    end
+
+    def forgot_password_email_preview
+        ExampleMailer.forgot_password_email(User.first.id)
     end
 end
