a40378e6f3d547327b2530d9b7ede9f99fefeb0c
Hakeem Almidan <hakeemmidan@gmail.com>
2019-12-02 20:44:08 -0800

    Create frontend route for ForgotPasswordForm component

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -4,6 +4,7 @@ import { Switch } from 'react-router-dom';
 import GreetingContainer from './greeting/greeting_container';
 import SignUpFormContainer from './auth/signup_form_container';
 import LogInFormContainer from './auth/login_form_container';
+import { ForgotPasswordForm } from './auth/ForgotPasswordForm';
 
 export const App = () => (
   <div>
@@ -14,6 +15,7 @@ export const App = () => (
     <Switch>
       <AuthRoute exact path="/login" component={LogInFormContainer} />
       <AuthRoute exact path="/signup" component={SignUpFormContainer} />
+      <AuthRoute exact path="/forgot-password" component={ForgotPasswordForm} />
     </Switch>
   </div>
 );
---
+++ frontend/components/auth/ForgotPasswordForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class ForgotPasswordForm extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello there
+            </div>
+        )
+    }
+}
