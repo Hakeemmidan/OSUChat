d8c4e7a1a4189cfc6e252ada3c7d20c91bbd5792
Hakeem Almidan <hakeemmidan@gmail.com>
2019-12-09 18:01:29 -0800

    Fix validation error (when resetting password of existing user)

--- app/models/user.rb
+++ app/models/user.rb
@@ -48,7 +48,7 @@ class User < ApplicationRecord
   def generate_password_token!
     self.reset_password_token = password_reset_token
     self.reset_password_sent_at = Time.now.utc
-    save!
+    save!(:validate => false)
   end
 
   def password_token_valid?
