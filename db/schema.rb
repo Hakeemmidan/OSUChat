14e59ac6523211ee1576fdf591ebfdf71441c1d0
Hakeem Almidan <hakeemmidan@gmail.com>
2019-12-19 23:59:51 -0800

    Add idxs on User username and email for safer / quicker validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_29_051342) do
+ActiveRecord::Schema.define(version: 2019_12_20_075740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 2019_11_29_051342) do
     t.string "confirm_token"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
+    t.index "lower((email)::text)", name: "user_lower_email_idx", unique: true
+    t.index "lower((username)::text)", name: "user_lower_username_idx", unique: true
     t.index ["session_token"], name: "index_users_on_session_token"
     t.index ["username"], name: "index_users_on_username"
   end
