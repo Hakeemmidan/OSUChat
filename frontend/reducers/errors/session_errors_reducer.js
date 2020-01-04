137b517f23abed601c9c30b680ecd0c0baf29013
Hakeem Almidan <hakeemmidan@gmail.com>
2020-01-03 23:46:43 -0800

    Remove unnecessary comments

--- frontend/reducers/errors/session_errors_reducer.js
+++ frontend/reducers/errors/session_errors_reducer.js
@@ -1,9 +1,7 @@
 import { RECEIVE_ERRORS } from '../../actions/session_actions';
 
-// keeps tracks of session-specific errors
 export const sessionErrorsReducer = (state = [], action) => {
   Object.freeze(state);
-  // return nothing if we hit default case
   const noErrors = [];
 
   switch (action.type) {
