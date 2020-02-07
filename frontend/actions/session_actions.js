adaf1ce5cd5bb2a21bd308e61e5faba5fe6d2814
Hakeem Almidan <hakeemmidan@gmail.com>
2020-02-06 18:30:05 -0800

    Change APIUtil to SessionAPIUtil in session_actions to be more specific

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -3,7 +3,7 @@ export const RECEIVE_ERRORS = 'RECEIVE_ERRORS';
 export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
 export const SIGNUP_CURRENT_USER = 'SIGNUP_CURRENT_USER';
 export const RECEIVE_FORGOT_PASSWORD = 'RECEIVE_FORGOT_PASSWORD';
-import * as APIUtil from '../util/session_api_util';
+import * as SessionAPIUtil from '../util/session_api_util';
 
 export const receiveCurrentUser = (currentUser) => ({
   type: RECEIVE_CURRENT_USER,
@@ -30,7 +30,7 @@ export const receiveErrors = errors => ({
 });
 
 export const signup = user => dispatch => (
-  APIUtil.signup(user).then(confirmationMsg => (
+  SessionAPIUtil.signup(user).then(confirmationMsg => (
     dispatch(signupCurrentUser(confirmationMsg))
   ), err => (
     dispatch(receiveErrors(err.responseJSON))
@@ -38,7 +38,7 @@ export const signup = user => dispatch => (
 );
 
 export const login = user => dispatch => (
-  APIUtil.login(user).then(user => (
+  SessionAPIUtil.login(user).then(user => (
     dispatch(receiveCurrentUser(user))
   ), err => (
     dispatch(receiveErrors(err.responseJSON))
@@ -46,13 +46,13 @@ export const login = user => dispatch => (
 );
 
 export const logout = () => dispatch => (
-  APIUtil.logout().then(() => (
+  SessionAPIUtil.logout().then(() => (
     dispatch(logoutCurrentUser())
   ))
 );
 
 export const forgotPassword = (email) => dispatch => (
-  APIUtil.forgotPassword(email).then((confirmationMsg) => (
+  SessionAPIUtil.forgotPassword(email).then((confirmationMsg) => (
     dispatch(receiveForgotPassword(confirmationMsg))
   ), err => (
     dispatch(receiveErrors(err.responseJSON))
