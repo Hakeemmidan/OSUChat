ede0aa40c85408eaf60e3af46c959eb8af008ca1
Hakeem Almidan <hakeemmidan@gmail.com>
2020-02-19 16:10:20 -0800

    Use Active jobs for confirmation email for faster site performance

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,7 +3,7 @@ class Api::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      UserMailer.confirmation_email(@user.id).deliver_now
+      SendConfirmationEmailJob.perform_later(@user.id)
       render json: ["Please check your email (#{@user.email})"]
     else
       render json: @user.errors.full_messages, status: 422
