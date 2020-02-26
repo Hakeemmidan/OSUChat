1dd69ca5bffe7c1b96911add301af2a959988574
Hakeem Almidan <hakeemmidan@gmail.com>
2020-02-26 14:51:34 -0800

    Create userOptionsModal skeleton

---
+++ frontend/components/modal/modal_options/user_options_modal.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export function UserOptionsModal() {
+    return (
+      <div>
+        <span>
+          Change Username
+        </span>
+
+        <span>
+          Delete account
+        </span>
+      </div>
+    )
+}
