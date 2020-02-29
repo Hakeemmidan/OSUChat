9976d55d1831baae293f05037797d46beace6927
Hakeem Almidan <hakeemmidan@gmail.com>
2020-02-28 23:56:09 -0800

    Add update username action case to confirmationReducer

--- frontend/reducers/ui/confirmation_reducer.js
+++ frontend/reducers/ui/confirmation_reducer.js
@@ -1,7 +1,5 @@
-import {
-    SIGNUP_CURRENT_USER,
-    RECEIVE_FORGOT_PASSWORD
-} from '../../actions/session_actions';
+import { SIGNUP_CURRENT_USER, RECEIVE_FORGOT_PASSWORD } from '../../actions/session_actions';
+import { RECEIVE_UPDATE_USERNAME } from "../../actions/user_actions";
 
 const confirmationReducer = (state = {}, action) => {
     let oldState = Object.freeze(state);
@@ -11,6 +9,8 @@ const confirmationReducer = (state = {}, action) => {
             return { signupConfirmation: action.confirmationMsg[0] }
         case RECEIVE_FORGOT_PASSWORD:
             return { forgotPasswordConfirmation: action.confirmationMsg[0] }
+        case RECEIVE_UPDATE_USERNAME:
+            return { updateUsernameConfirmation: action.confirmationMsg[0] }
         default:
             return oldState;
     }
