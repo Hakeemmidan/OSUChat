23b930afbcc8192f5b8813a2a7df6e4afd21054c
Hakeem Almidan <hakeemmidan@gmail.com>
2020-03-07 23:18:35 -0800

    Fix indentation for confirmation reducer

--- frontend/reducers/ui/confirmation_reducer.js
+++ frontend/reducers/ui/confirmation_reducer.js
@@ -2,18 +2,18 @@ import { SIGNUP_CURRENT_USER, RECEIVE_FORGOT_PASSWORD } from '../../actions/sess
 import { RECEIVE_UPDATE_USERNAME } from "../../actions/user_actions";
 
 const confirmationReducer = (state = {}, action) => {
-    let oldState = Object.freeze(state);
+  let oldState = Object.freeze(state);
 
-    switch (action.type) {
-        case SIGNUP_CURRENT_USER:
-            return { signupConfirmation: action.confirmationMsg[0] }
-        case RECEIVE_FORGOT_PASSWORD:
-            return { forgotPasswordConfirmation: action.confirmationMsg[0] }
-        case RECEIVE_UPDATE_USERNAME:
-            return { updateUsernameConfirmation: action.confirmationMsg[0] }
-        default:
-            return oldState;
-    }
+  switch (action.type) {
+    case SIGNUP_CURRENT_USER:
+      return { signupConfirmation: action.confirmationMsg[0] }
+    case RECEIVE_FORGOT_PASSWORD:
+      return { forgotPasswordConfirmation: action.confirmationMsg[0] }
+    case RECEIVE_UPDATE_USERNAME:
+      return { updateUsernameConfirmation: action.confirmationMsg[0] }
+    default:
+      return oldState;
+  }
 };
 
 export default confirmationReducer;
