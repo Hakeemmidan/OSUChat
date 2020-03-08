0be1df4281690534e4aa586aded8c59d7f81e85d
Hakeem Almidan <hakeemmidan@gmail.com>
2020-03-07 23:24:50 -0800

    Change UnknownRoute component styling from inline to class based for less specificity

--- frontend/components/unknown_route/UnknownRoute.jsx
+++ frontend/components/unknown_route/UnknownRoute.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export const UnknownRoute = () => (
   // Will get displayed if the user tries to go to a route that doesn't exist
-    <div style={{ display: 'flex', paddingTop: '60px' }}>
-      <span style={{ margin: 'auto' }}>
+    <div className="unknown-route__container">
+      <span className="unknown-route__inner">
         You entered a route that doesn't exist
         <br/>
         Click on logo to navigate back
