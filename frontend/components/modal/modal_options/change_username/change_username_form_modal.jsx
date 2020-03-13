7d19ee97e95a4663c62bd0f369d97b2ed17024e0
Hakeem Almidan <hakeemmidan@gmail.com>
2020-03-12 23:14:14 -0700

    Create renderConfirmation() renderErrors() and update() methods in ChangeUsernameFormModal

--- frontend/components/modal/modal_options/change_username/change_username_form_modal.jsx
+++ frontend/components/modal/modal_options/change_username/change_username_form_modal.jsx
@@ -7,12 +7,52 @@ export class ChangeUsernameFormModal extends React.Component {
     this.state = {
       newUsername: ""
     };
+    this.displayErrors = false;
+    this.displayConfirmation = false;
   }
   
-  handleSubmit() {
-    this.props.processForm(this.state.newUsername)
+  update(field) {
+    return e => this.setState({
+      [field]: e.currentTarget.value
+    });
+  }
+  
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.processForm(this.state.newUsername);
+    this.displayErrors = true;
+    this.displayConfirmation = true;
   }
 
+  renderConfirmation() {
+    if (this.displayConfirmation && this.props.signupConfirmation) {
+      return (
+        <ul>
+          <li key="forgotPasswordConfirmation" className="session__msg--confirmation">
+            {this.props.forgotPasswordConfirmation}
+          </li>
+        </ul>
+      )
+    } else {
+      return null
+    }
+  }
+
+  renderErrors() {
+    if (this.displayErrors) {
+      return (
+        <ul>
+          {this.props.errors.map((error, i) => (
+            <li key={`error-${i}`} className="session__msg--error">
+              {error}
+            </li>
+          ))}
+        </ul>
+      )
+    } else {
+      return null
+    }
+  }
 
   render() {
 
