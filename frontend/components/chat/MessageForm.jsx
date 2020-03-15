123a53a7abb9547b04fe1839a8d83bba4f949f02
Hakeem Almidan <hakeemmidan@gmail.com>
2020-03-15 00:03:27 -0700

    Delete es-lint prop types disablement

--- frontend/components/chat/MessageForm.jsx
+++ frontend/components/chat/MessageForm.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/prop-types */
 import React from "react";
 
 export class MessageForm extends React.Component {
--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/prop-types */
 import React from 'react';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
