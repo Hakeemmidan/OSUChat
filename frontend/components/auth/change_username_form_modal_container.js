5c5f6285ec059faba0ced67090188c0fcc40533f
Hakeem Almidan <hakeemmidan@gmail.com>
2020-03-24 15:56:33 -0700

    Update content of change_username_form_modal_container to meet SingleFieldForm standards

---
+++ frontend/components/auth/change_username_form_modal_container.js
@@ -0,0 +1,21 @@
+import { connect } from 'react-redux';
+import { updateUsername } from '../../actions/user_actions';
+import { SingleFormField } from './SingleFieldForm';
+
+const mapStateToProps = (state) => {
+  return {
+    errors: state.errors.user,
+    confirmation: state.ui.confirmation.updateUsernameConfirmation,
+    instructions: "Reset username:",
+    fieldLabel: null,
+    submitButtonText: "Reset username"
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    processForm: (username) => dispatch(updateUsername(username)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SingleFormField);
