f59043c5f0c76226f8861c49767f7799881bed50
Hakeem Almidan <hakeemmidan@gmail.com>
2020-03-26 21:49:00 -0700

    Make clicking on change username in user options modal navigate to '/change-username' and close modal

--- frontend/components/modal/modal_options/user_options/user_options_modal.jsx
+++ frontend/components/modal/modal_options/user_options/user_options_modal.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export function UserOptionsModal(props) {
     return (
       <div>
-        <p className="link" onClick={e => {
-          e.stopPropagation
-          props.openModal('changeUsername')
+        <p onClick={e => {
+          e.preventDefault();
+          e.stopPropagation();
+          props.closeModal();
         }}>
-          Change Username
+          <Link className="link" to="/change-username">
+            Change Username
+          </Link> 
+
         </p>
 
         <p className="link__red user-options__delete-account">
--- frontend/components/modal/modal_options/user_options/user_options_modal_container.js
+++ frontend/components/modal/modal_options/user_options/user_options_modal_container.js
@@ -1,10 +1,9 @@
 import { connect } from 'react-redux';
-import { openModal, closeModal } from '../../../../actions/modal_actions';
+import { closeModal } from '../../../../actions/modal_actions';
 import { UserOptionsModal } from './user_options_modal';
 
 const mapDispatchToProps = dispatch => {
   return {
-    openModal: (modal) => dispatch(openModal(modal)),
     closeModal: () => dispatch(closeModal())
   };
 };
