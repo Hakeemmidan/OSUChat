4d181295394d8c04ceed32300c42d955efcb0331
Hakeem Almidan <hakeemmidan@gmail.com>
2020-04-02 22:00:54 -0700

    Fix param selection issue in UsersController#destroy

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -11,7 +11,7 @@ class Api::UsersController < ApplicationController
   end
 
   def destroy
-    user = User.find(user_params[:id])
+    user = User.find(params[:id])
 
     if user
       user.destroy
