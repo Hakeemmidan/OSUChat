f55aae389783aeda54542aabbe53333503779ca0
Hakeem Almidan <hakeemmidan@gmail.com>
2020-04-02 21:47:46 -0700

    Map currentUser to props of UserOptionsModal

--- frontend/components/modal/modal_options/user_options/user_options_modal_container.js
+++ frontend/components/modal/modal_options/user_options/user_options_modal_container.js
@@ -2,10 +2,16 @@ import { connect } from 'react-redux';
 import { closeModal } from '../../../../actions/modal_actions';
 import { UserOptionsModal } from './user_options_modal';
 
+const mapStateToProps = ({ entities, session }) => {
+  return {
+    currentUser: entities.users[session.id]
+  };
+};
+
 const mapDispatchToProps = dispatch => {
   return {
     closeModal: () => dispatch(closeModal())
   };
 };
 
-export default connect(null, mapDispatchToProps)(UserOptionsModal);
+export default connect(mapStateToProps, mapDispatchToProps)(UserOptionsModal);
