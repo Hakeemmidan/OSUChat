d2d0efe9053cfbbfc5ee6c19b63af18d93d73b35
Hakeem Almidan <hakeemmidan@gmail.com>
2020-04-10 20:44:58 -0700

    Style .img__ciao

--- frontend/components/modal/modal_options/ciao/Ciao.jsx
+++ frontend/components/modal/modal_options/ciao/Ciao.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 export function Ciao() {
   return (
     <img
+      className="img__ciao"
       src={window.ciaoImagePath}
       alt="Ciao! [Ps: Know that you're welcome to come back anytime]"
     />
