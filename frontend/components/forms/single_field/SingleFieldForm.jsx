bf9a9459116aa7e11901f8a8338f7689de20f16a
Hakeem Almidan <hakeemmidan@gmail.com>
2020-04-14 15:20:14 -0700

    Further refactor SingleFieldForm

--- frontend/components/forms/single_field/SingleFieldForm.jsx
+++ frontend/components/forms/single_field/SingleFieldForm.jsx
@@ -1,101 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-export class SingleFieldForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            body: ''
-        }
-        this.displayErrors = false;
-        this.displayConfirmation = false;
-        this.update = this.update.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.renderConfirmation = this.renderConfirmation.bind(this);
-        this.renderErrors = this.renderErrors.bind(this);
+export const SingleFieldForm = (props) => {
+  const [body, useBody] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (props.type === 'change-username') {
+      props.processForm(props.currentUser.id, body);
+    } else if (props.type === 'forgot-password') {
+      props.processForm(body)
     }
+  }
 
-    update(field) {
-        return e => this.setState({
-            [field]: e.currentTarget.value
-        });
-    }
+  const renderConfirmation = () => (
+    props.confirmation ?
+      <ul>
+        <li key="SingleFormField-confirmation" className="session__msg--confirmation">
+          {props.confirmation}
+        </li>
+      </ul>
+    : null
+  )
 
-    handleSubmit(e) {
-        e.preventDefault();
-        if (this.props.type === 'change-username') {
-          this.props.processForm(this.props.currentUser.id, this.state.body);
-        } else {
-          this.props.processForm(this.state.body)
-        }
-        this.displayErrors = true;
-        this.displayConfirmation = true;
-    }
+  const renderErrors = () => (
+    props.errors.length > 0 ?
+      <ul className="u-no-padding">
+        {props.errors.map((error, i) => (
+          <li key={`error-${i}`} className="session__msg--error">
+            {error}
+          </li>
+        ))}
+      </ul>
+    : null
+  )
 
-    renderConfirmation() {
-      if (this.displayConfirmation && this.props.confirmation) {
-            return (
-                <ul>
-                    <li key="SingleFormField-confirmation" className="session__msg--confirmation">
-                        {this.props.confirmation}
-                    </li>
-                </ul>
-            )
-        } else {
-            return null
-        }
-    }
+  return (
+    <div className="session__form-container">
+      <div className="session__form-box">
+        <form onSubmit={handleSubmit}>
+          {props.instructions}
 
-    renderErrors() {
-        if (this.displayErrors) {
-            return (
-                <ul className="u-no-padding">
-                    {this.props.errors.map((error, i) => (
-                        <li key={`error-${i}`} className="session__msg--error">
-                            {error}
-                        </li>
-                    ))}
-                </ul>
-            )
-        } else {
-            return null
-        }
-    }
+          <br />
 
-    render() {
-        return (
-            <div className="session__form-container">
-                <div className="session__form-box">
-                    <form onSubmit={this.handleSubmit}>
-                        {this.props.instructions}
+          {renderConfirmation()}
+          {renderErrors()}
 
-                        <br/>
+          <br />
 
-                        {this.renderConfirmation()}
-                        {this.renderErrors()}
+          <label className="session__input-container">
+            {props.fieldLabel}
+            <br />
+            <input type="text"
+              value={body}
+              onChange={e => useBody(e.currentTarget.value)}
+              className="session__textbox"
+            />
+          </label>
 
-                        <br/>
+          <br />
 
-                        <label className="session__input-container">
-                            {this.props.fieldLabel}
-                            <br />
-                            <input type="text"
-                                value={this.state.body}
-                                onChange={this.update('body')}
-                                className="session__textbox"
-                            />
-                        </label>
-
-                        <br/>
-
-                        <div className="session__btn--submit" onClick={this.handleSubmit}>
-                            <input type="submit" value={this.props.submitButtonText} />
-                        </div>
-                    </form>
-                </div>
-            </div>
-        )
-    }
+          <div className="session__btn--submit" onClick={handleSubmit}>
+            <input type="submit" value={props.submitButtonText} />
+          </div>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 SingleFieldForm.propTypes = {
